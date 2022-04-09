6921dcfb6ea73f19650359dc164c0034df252837
quantumsheep <nathanael.dmc@outlook.fr>
2022-04-09 22:29:08 +0200

    Implement controller base structure

---
+++ controller.go
@@ -0,0 +1,34 @@
+package plouf
+
+import (
+	"reflect"
+
+	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
+)
+
+type IController interface {
+	IInjectable
+
+	InitRoutes(e *echo.Echo)
+}
+
+type Controller struct {
+	Injectable
+}
+
+func (c Controller) Init(self IInjectable) error {
+	if err := c.Injectable.Init(self); err != nil {
+		return err
+	}
+
+	if c.ShouldLogInjection(self) {
+		logrus.Debugf("Initializing controller %s", reflectTypeName(self))
+	}
+
+	return nil
+}
+
+func (c Controller) ShouldLogInjection(self IInjectable) bool {
+	return reflectValue(self).Type() != reflect.TypeOf(Controller{})
+}
