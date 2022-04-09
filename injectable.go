6fef0354db1203c3cef627ea38f40f8093ffb409
quantumsheep <nathanael.dmc@outlook.fr>
2022-04-10 01:26:14 +0200

    Encapsulate injection system in worker structure

--- injectable.go
+++ injectable.go
@@ -1,20 +1,27 @@
 package plouf
 
-import (
-	"reflect"
-
-	"github.com/sirupsen/logrus"
-)
-
 type IInjectable interface {
 	Init(self IInjectable) error
 
 	ShouldLogInjection(self IInjectable) bool
+	IsInitialized() bool
 }
 
-type Injectable struct{}
+type Injectable struct {
+	initialized bool
+}
+
+func (i *Injectable) IsInitialized() bool {
+	return i.initialized
+}
 
 func (i *Injectable) Init(self IInjectable) error {
+	if i.IsInitialized() {
+		return nil
+	}
+
+	i.initialized = true
+
 	value := ReflectValue(self)
 	for i := 0; i < value.NumField(); i++ {
 		field := value.Field(i)
@@ -24,7 +31,7 @@ func (i *Injectable) Init(self IInjectable) error {
 		}
 
 		if injectable, ok := field.Interface().(IInjectable); ok {
-			if wasInitialized := markInjectableAsInitialized(field); wasInitialized {
+			if injectable.IsInitialized() {
 				continue
 			}
 
@@ -39,79 +46,4 @@ func (i *Injectable) Init(self IInjectable) error {
 
 func (i *Injectable) ShouldLogInjection(self IInjectable) bool {
 	return false
-}
-
-type injectableStatus struct {
-	Injectable  IInjectable
-	Initialized bool
-}
-
-var injectables = make(map[reflect.Type]*injectableStatus)
-
-func markInjectableAsInitialized(field reflect.Value) (wasInitialized bool) {
-	if field.Kind() == reflect.Ptr {
-		if status, ok := injectables[field.Type().Elem()]; ok {
-			if status.Initialized {
-				return true
-			}
-
-			status.Initialized = true
-		}
-	}
-
-	return false
-}
-
-func Inject(injectable IInjectable) error {
-	if injectable.ShouldLogInjection(injectable) {
-		logrus.Debugf("Injecting %s", reflect.TypeOf(injectable).Elem().Name())
-	}
-
-	value := reflect.ValueOf(injectable).Elem()
-	for i := 0; i < value.NumField(); i++ {
-		field := value.Field(i)
-
-		if !field.CanInterface() {
-			continue
-		}
-
-		if _, ok := field.Interface().(IInjectable); !ok {
-			continue
-		}
-
-		typ := field.Type()
-
-		var value reflect.Value
-
-		// Use cache if possible, else assign new value
-		if typ.Kind() == reflect.Ptr {
-			if value, ok := injectables[typ.Elem()]; ok {
-				field.Set(reflect.ValueOf(value))
-				continue
-			}
-
-			value = reflect.New(typ.Elem())
-		} else {
-			value = reflect.New(typ)
-		}
-
-		// Save new value to cache and set field value
-		if typ.Kind() == reflect.Ptr {
-			injectables[typ.Elem()] = &injectableStatus{
-				Injectable:  value.Interface().(IInjectable),
-				Initialized: false,
-			}
-			field.Set(value)
-		} else {
-			field.Set(value.Elem())
-		}
-
-		// Inject new value dependencies
-		child := value.Interface().(IInjectable)
-		if err := Inject(child); err != nil {
-			return err
-		}
-	}
-
-	return nil
 }
--- worker.go
+++ worker.go
@@ -2,20 +2,35 @@ package plouf
 
 import (
 	"net/http"
+	"reflect"
 
 	"github.com/go-playground/validator"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/sirupsen/logrus"
 )
 
 type Worker struct {
 	e *echo.Echo
 
-	mainModule IModule
+	injectables map[reflect.Type]*injectableStatus
+	mainModule  IModule
 }
 
 func NewWorker(mainModule IModule) (*Worker, error) {
-	if err := Inject(mainModule); err != nil {
+	e := echo.New()
+	e.HideBanner = true
+	e.Validator = NewDTOValidator()
+
+	e.Use(middleware.Recover())
+
+	worker := &Worker{
+		e:           e,
+		injectables: make(map[reflect.Type]*injectableStatus),
+		mainModule:  mainModule,
+	}
+
+	if err := worker.Inject(mainModule); err != nil {
 		return nil, err
 	}
 
@@ -23,24 +38,88 @@ func NewWorker(mainModule IModule) (*Worker, error) {
 		return nil, err
 	}
 
-	e := echo.New()
-	e.HideBanner = true
-	e.Validator = NewDTOValidator()
-
-	e.Use(middleware.Recover())
-
 	if err := mainModule.InitControllersRoutes(mainModule, e); err != nil {
 		return nil, err
 	}
 
-	return &Worker{
-		e:          e,
-		mainModule: mainModule,
-	}, nil
+	return worker, nil
 }
 
 func (w *Worker) Start(address string) error {
 	return w.e.Start(address)
+}
+
+type injectableStatus struct {
+	Injectable  IInjectable
+	Initialized bool
+}
+
+func (w *Worker) markInjectableAsInitialized(field reflect.Value) (wasInitialized bool) {
+	if field.Kind() == reflect.Ptr {
+		if status, ok := w.injectables[field.Type().Elem()]; ok {
+			if status.Initialized {
+				return true
+			}
+
+			status.Initialized = true
+		}
+	}
+
+	return false
+}
+
+func (w *Worker) Inject(injectable IInjectable) error {
+	if injectable.ShouldLogInjection(injectable) {
+		logrus.Debugf("Injecting %s", reflect.TypeOf(injectable).Elem().Name())
+	}
+
+	value := reflect.ValueOf(injectable).Elem()
+	for i := 0; i < value.NumField(); i++ {
+		field := value.Field(i)
+
+		if !field.CanInterface() {
+			continue
+		}
+
+		if _, ok := field.Interface().(IInjectable); !ok {
+			continue
+		}
+
+		typ := field.Type()
+
+		var value reflect.Value
+
+		// Use cache if possible, else assign new value
+		if typ.Kind() == reflect.Ptr {
+			if value, ok := w.injectables[typ.Elem()]; ok {
+				field.Set(reflect.ValueOf(value))
+				continue
+			}
+
+			value = reflect.New(typ.Elem())
+		} else {
+			value = reflect.New(typ)
+		}
+
+		// Save new value to cache and set field value
+		if typ.Kind() == reflect.Ptr {
+			w.injectables[typ.Elem()] = &injectableStatus{
+				Injectable:  value.Interface().(IInjectable),
+				Initialized: false,
+			}
+			field.Set(value)
+		} else {
+			field.Set(value.Elem())
+		}
+
+		// Inject new value dependencies
+		child := value.Interface().(IInjectable)
+		if err := w.Inject(child); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 type DTOValidator struct {
