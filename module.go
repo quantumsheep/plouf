97e964c8ed3554b712d2b2cd75a58f21fed01d71
quantumsheep <nathanael.dmc@outlook.fr>
2022-04-09 22:30:10 +0200

    Implement module base structure

---
+++ module.go
@@ -0,0 +1,60 @@
+package plouf
+
+import (
+	"reflect"
+
+	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
+)
+
+type IModule interface {
+	IInjectable
+
+	InitControllersRoutes(self IInjectable, e *echo.Echo) error
+}
+
+type Module struct {
+	Injectable
+}
+
+func (m Module) Init(self IInjectable) error {
+	if err := m.Injectable.Init(self); err != nil {
+		return err
+	}
+
+	if m.ShouldLogInjection(self) {
+		logrus.Debugf("Initializing module %s", reflectTypeName(self))
+	}
+
+	return nil
+}
+
+func (m Module) ShouldLogInjection(self IInjectable) bool {
+	return reflectValue(self).Type() != reflect.TypeOf(Module{})
+}
+
+func (m Module) InitControllersRoutes(self IInjectable, e *echo.Echo) error {
+	value := reflectValue(self)
+	for i := 0; i < value.NumField(); i++ {
+		field := value.Field(i)
+
+		if !field.CanInterface() {
+			continue
+		}
+
+		if _, ok := field.Interface().(IInjectable); ok {
+			if controller, ok := field.Interface().(IController); ok {
+				logrus.Debugf("Initializing routes for %s", reflectTypeName(controller))
+				controller.InitRoutes(e)
+			}
+
+			if module, ok := field.Interface().(IModule); ok {
+				if err := module.InitControllersRoutes(module, e); err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	return nil
+}
